5b994319d68d06032313de762390cb17010d7f1d
Daniel <dannyamah@gmail.com>
2022-12-21 14:37:48 -0800

    login page added

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -15,7 +15,9 @@
             <!-- Email Address -->
             <div>
                 <x-input-label for="email" :value="__('Email')" />
+
                 <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
@@ -52,4 +54,123 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
+
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+
+<!-- Mirrored from pixelwibes.com/template/ebazar/html/dist/ui-elements/auth-signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 09 Nov 2021 20:16:40 GMT -->
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Light Express </title>
+    <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
+
+    <!-- project css file  -->
+    <link rel="stylesheet" href="/d_assets/css/ebazar.style.min.css">
+</head>
+
+<body>
+    <div id="ebazar-layout" class="theme-blue">
+
+        <!-- main body area -->
+        <div class="main p-2 py-3 p-xl-5 ">
+            <!-- Body: Body -->
+            <div class="body d-flex p-0 p-xl-5">
+                <div class="container-xxl">
+
+                    <div class="row g-0">
+                        <div
+                            class="col-lg-6 d-none d-lg-flex justify-content-center align-items-center rounded-lg auth-h100">
+                            <div style="max-width: 25rem;">
+                                <div class="text-center mb-5">
+                                    <i class="bi bi-bag-check-fill  text-primary" style="font-size: 90px;"></i>
+                                </div>
+                                <div class="mb-5">
+                                    <h2 class="color-900 text-center">A few clicks is all it takes.</h2>
+                                </div>
+                                <!-- Image block -->
+                                <div class="">
+                                    <img src="/d_assets/images/login-img.svg" alt="login-img">
+                                </div>
+                            </div>
+                        </div>
+
+                        <div
+                            class="col-lg-6 d-flex justify-content-center align-items-center border-0 rounded-lg auth-h100">
+                            <div class="w-100 p-3 p-md-5 card border-0 shadow-sm" style="max-width: 32rem;">
+                                <!-- Form -->
+                                <form class="row g-1 p-3 p-md-4" action="{{ route('login') }}" method="POST"> @csrf
+                                    <div class="col-12 text-center mb-5">
+                                        <h1>Sign in</h1>
+                                        <span>Free access to our dashboard.</span>
+                                    </div>
+                                    <div class="col-12 text-center mb-4">
+                                        <a class="btn btn-lg btn-light btn-block" href="">
+                                            <span class="d-flex justify-content-center align-items-center">
+                                                <img class="avatar xs me-2" src="/d_assets/images/google.svg"
+                                                    alt="Image Description">
+                                                Sign in with Google
+                                            </span>
+                                        </a>
+                                        <span class="dividers text-muted mt-4">OR</span>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="mb-2">
+                                            <label class="form-label">Email address</label>
+                                            <input type="email" name="email" class="form-control form-control-lg"
+                                                placeholder="name@example.com">
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="mb-2">
+                                            <label class="form-label">Password</label>
+                                            <input type="password" name="password" class="form-control form-control-lg"
+                                                placeholder="*********">
+                                        </div>
+                                    </div>
+                                    <div class="col-6  mt-2">
+                                        @if (Route::has('password.request'))
+                                            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                                                href="{{ route('password.request') }}">
+                                                {{ __('Forgot your password?') }}
+                                            </a>
+                                        @endif
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="form-check">
+
+
+                                        </div>
+                                    </div>
+                                    <div class="col-12 text-center mt-4">
+                                        <button class="btn btn-lg btn-block btn-light lift text-uppercase">SIGN
+                                            IN</button>
+                                    </div>
+
+                                    <div class="col-12 text-center mt-4">
+                                        <span>Don't have an account yet? <a href="{{ route('register') }}"
+                                                class="text-secondary">Sign up here</a></span>
+                                    </div>
+                                </form>
+                                <!-- End Form -->
+
+                            </div>
+                        </div>
+                    </div> <!-- End Row -->
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!-- Jquery Core Js -->
+    <script src="/d_assets/bundles/libscripts.bundle.js"></script>
+</body>
+
+<!-- Mirrored from pixelwibes.com/template/ebazar/html/dist/ui-elements/auth-signin.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 09 Nov 2021 20:16:42 GMT -->
+
+</html>
